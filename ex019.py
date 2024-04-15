17ffa6a326d90ea1f12cef7a3362fefa6b2c0ea8
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-04-15 21:48:08 +0100

    ex

--- ex019.py
+++ ex019.py
@@ -3,5 +3,5 @@ students = []
 i = 1
 while (i <= 4):
     students.append(str(input("{}ª student's name: ".format(i))))
-    i += 1
+    i += 2
 print("Chosen student: {}".format(choice(students)))
--- ex020.py
+++ ex020.py
@@ -3,6 +3,6 @@ students = []
 i = 1
 while (i <= 4):
     students.append(str(input("{}ª student's name: ".format(i))))
-    i += 1
+    i += 2
 shuffle(students)
 print("Presentation order: {}".format(students))
