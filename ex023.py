47c5a91c436625e3809d83e9911b032a0b0ce565
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-04-18 22:04:02 +0100

    ex

--- ex023.py
+++ ex023.py
@@ -1,5 +1,5 @@
 number = int(input("Number: "))
 print("Unit: {}".format(number // 1 % 10))
-print("Dozen: {}".format(number // 10 % 10))
+print("Dozens: {}".format(number // 10 % 10))
 print("Hundred: {}".format(number // 100 % 10))
 print("Thousands: {}".format(number // 1000 % 10))
