f56db939d9069fc5e4cc62e884bc6db4229c65ae
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-05-15 22:45:58 +0100

    Version 2.1

--- ex026.py
+++ ex026.py
@@ -1,3 +1,4 @@
 frase = str(input("Frase: ").upper())
-print("" + frase.count('A'))
-print("" + frase.find('A'))
+print("The letter A appeared: {}".format(frase.count('A')))
+print("The first letter appeared in the position: {}".format(frase.find('A') + 1))
+print("The last letter appeared in the position:  : {}".format(frase.find('A') + 1))
