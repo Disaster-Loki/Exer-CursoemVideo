63804afd243711e7ad286e763d8987fc746ba83f
Sebastião De Carvalho <sebastiaol.carvalho22@gmail.com>
2024-05-15 23:26:50 +0100

    Version 2.1

--- ex026.py
+++ ex026.py
@@ -1,4 +1,4 @@
-frase = str(input("Fras: ").upper().strip())
+frase = str(input("Frase: ").upper().strip())
 print("The letter A appeared: {}".format(frase.count('A')))
 print("The first letter appeared in the position: {}".format(frase.find('A') + 1))
 print("The last letter appeared in the position:  : {}".format(frase.rfind('A') + 1))
